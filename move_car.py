b3969cd34b0dfdf6d8b940979fe9634bc8f2a511
Michael Levy <malevy@ucdavis.edu>
2017-02-23 14:27:57 -0800

    reviewed code

--- move_car.py
+++ move_car.py
@@ -1,18 +1,28 @@
+# Do you need to keep `directions` as a separate item? Could you not just us the keys to `movement`?
 directions = ['N','E','S','W'] 
 movement = {'N': (0,1), 'E': (1,0), 'S': (0,-1), 'W':(-1,0)}
 commands = {'L': 'turn_left', 'R': 'turn_right', 'M': 'move'}
 
+# Grouping the input-procssing lines together would make the program flow clearer
 GRID_MAX_X, GRID_MAX_Y = map(int, raw_input().split())
 
+# I don't think these variables do anything. If they do, consider the following comments:
+# Why are second vehicle positions not initialized here or updated below? 
+# Consider storing x and y locations as a tuple to keep them together.
+# Do they need to be initialized with missing values? Why not just initialize with the input?
 first_vehicle_x = None
 first_vehicle_y = None
 
+# Put class definitions near the top
 class Vehicle():
     def __init__(self, x, y, face):
         self.x = x
         self.y = y
         self.dir = face
 
+    # These turn functions are opaque to me. Can you clarify by simplifying
+    # their operation or add a comment?
+    # Also, write unit tests for each of these functions.
     def turn_left(self):
         self.dir = directions[(directions.index(self.dir)-1)%len(directions)]
 
@@ -26,9 +36,11 @@ class Vehicle():
         if new_x != first_vehicle_x or new_y != first_vehicle_y:
             if new_x in xrange(GRID_MAX_X+1):
                 self.x = new_x
-            if new_y in xrange(GRID_MAX_Y+1):
+            if new_y in xrange(GRID_MAX_Y+1):  # This should be `yrange`
                 self.y = new_y
 
+# There's a lot of duplicate code for each vehicle here. Could you put it in a function?
+# Better yet, is anything needed besides the intiialization, updating, and print lines for each vehicle?
 vehicle_one_pos = raw_input().split()
 vehicle_one_commands = raw_input()
 
